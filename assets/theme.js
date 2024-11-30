0bdcf5c2bbf46e267598123e888817b93515f780
aniual <hpraniu@163.com>
2024-11-30 23:22:04 +0800

    add theme

--- assets/theme.js
+++ assets/theme.js
@@ -719,15 +719,29 @@
                     e.stopPropagation();
                     var $target = $(this);
                     var $parent = $target.parent();
-                    var $menuDislosure1 = $target.parent().find('ul.list-menu--disclosure-1');
+                    // var $menuDislosure1 = $target.parent().find('ul.list-menu--disclosure-1');
+                    var $submenu = $target.parent().find('ul.list-menu--disclosure-mobile-1');
 
-                    $parent.removeClass('is-hidden').addClass('is-open').removeClass('d-none');
-                    $menuDislosure1.off('transitionend.toggleMenuLink1').on('transitionend.toggleMenuLink1', () => {
-                        if ($parent.hasClass('is-open') && !$parent.hasClass('is-hidden') && !$parent.hasClass('d-none')) {
-                            // $parent.addClass('d-none')
-                            $parent.siblings().removeClass('is-open').addClass('is-hidden').removeClass('d-none');
-                        }
-                    })
+                    $submenu.removeClass('list-menu--disclosure-1 header__submenu list-menu list-menu--disclosure').addClass('mobile-menu-class');
+
+                    // 控制显示或隐藏子菜单
+                    if ($submenu.is(':visible')) {
+                        // 隐藏子菜单并移除类
+                        $submenu.slideUp().removeClass('mobile-menu-class');
+                    } else {
+                        // 展开子菜单并添加类
+                        $submenu.slideDown().addClass('mobile-menu-class');
+                        // 隐藏其他兄弟菜单的子菜单
+                        $parent.siblings().find('ul.list-menu--disclosure-1').slideUp().removeClass('mobile-menu-class');
+                    }
+
+                    // $parent.removeClass('is-hidden').addClass('is-open').removeClass('d-none');
+                    // $menuDislosure1.off('transitionend.toggleMenuLink1').on('transitionend.toggleMenuLink1', () => {
+                    //     if ($parent.hasClass('is-open') && !$parent.hasClass('is-hidden') && !$parent.hasClass('d-none')) {
+                    //         // $parent.addClass('d-none')
+                    //         $parent.siblings().removeClass('is-open').addClass('is-hidden').removeClass('d-none');
+                    //     }
+                    // })
 
                     // $target.parent().siblings().removeClass('is-open').addClass('is-hidden');
                     // $target.parent().removeClass('is-hidden').addClass('is-open');
